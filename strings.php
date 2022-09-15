867c2d195526bf72cf807fc7021a3ce8b1c821be
luvsouljuh <mateusz.mikosik@wp.pl>
2022-09-15 12:11:54 +0200

    syntax variables i css przed stringami

--- strings.php
+++ strings.php
@@ -1,27 +1,13 @@
 <!doctype html>
-<html lang="pl">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="main.css">
 </head>
 <body>
-    <?php
-    $title = 'Strona główna';
-    ?>
-    <h1> <?php echo $title; ?> </h1>
-    </div>
-
-    <ol>
-        <li> <a href="syntax.php"> składnia </li>
-        <li> <a href="variables.php"> zmienne </li>
-    </ol>
-
-
-
 
 </body>
 </html>
--- variables.php
+++ variables.php
@@ -1,1 +1,85 @@
-<?php
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Variables </title>
+</head>
+<body>
+    <h5> string (ciąg znaków) </h5>
+    <?php
+        $ciag_znakow = 'ciąg znaków';
+        ?>
+    <?= $ciag_znakow ; ?>
+
+    <hr>
+    <h5> int (liczba całkowita) </h5>
+    <?php
+        $licza_całkowita= 212323232;
+        ?>
+    <?= $licza_całkowita ; ?>
+
+    <hr>
+    <h5> float (liczba zmiennoprzecinkowa) </h5>
+    <?php
+        $l_zmiennoprzecinkowa = 232323232.2323;
+        ?>
+    <?= $l_zmiennoprzecinkowa ; ?>
+
+    <hr>
+
+    <h5> boolean </h5>
+    <?php
+        $prawdafałsz = true;
+        ?>
+    <?= $prawdafałsz ; ?>
+    <hr>
+    <h5> wyświetlanie za pomocą echo/var_dump </h5>
+    <?php
+        echo $ciag_znakow;
+        ?>
+    <br>
+    <br>
+    <?php
+        var_dump($licza_całkowita);
+        ?>
+    <br>
+    <br>
+    <?php
+        echo $l_zmiennoprzecinkowa;
+        ?>
+    <br>
+    <br>
+    <?php
+        var_dump($prawdafałsz);
+        ?>
+    <br>
+    <hr>
+    <h5> stała (define) </h5>
+    <a> define("moj_numer", 2137)</a>
+    <br>
+    <br>
+    <?php
+        define("moj_numer", 2137);
+        ?>
+    <hr>
+    <h5> wyświetlanie stałej za pomocą print i var_export </h5>
+    <?php
+        print moj_numer;
+        ?>
+    <br>
+    <br>
+    <?php
+        var_export(moj_numer);
+        ?>
+    <hr>
+
+
+
+
+
+
+</body>
+</html>
